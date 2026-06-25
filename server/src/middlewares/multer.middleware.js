2e7599699320ed822ace5592f990bfd0d1e0b1ed
Apoorv Gupta <guptaapoorv2005@gmail.com>
2026-06-26 00:02:56 +0530

    feat: add file filter to multer middleware for image file validation

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -11,4 +11,12 @@ const storage = multer.diskStorage({
   }
 })
 
-export const upload = multer({ storage: storage })
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only image files are allowed"), false);
+    }
+};
+
+export const upload = multer({ storage, fileFilter });
