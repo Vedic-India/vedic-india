c1f48ef1d3c7d92f5b75a923b7e8beb596c8d710
Apoorv Gupta <guptaapoorv2005@gmail.com>
2026-06-29 23:57:03 +0530

    feat: add optional JWT authentication middleware for user verification

---
+++ server/src/middlewares/optionalAuth.middleware.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+
+
+export const verifyJWT = asyncHandler(async (req, res, next)=>{
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
+    if(!token){
+        return next()
+    }
+
+    let decodedToken
+    try {
+        decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    } catch (error) {
+        return next()
+    }
+
+    const user = await User.findById(decodedToken._id)
+    if(!user){
+        return next()
+    }
+
+    req.user = user
+
+    next()
+})
