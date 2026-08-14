ebca36f29e1ff0343ebb3441a7d08334c3ea1af7
Apoorv Gupta <guptaapoorv2005@gmail.com>
2026-08-14 23:31:35 +0530

    refactor(sendEmail): remove Gmail connection test and update SMTP configuration for Brevo

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,52 +1,15 @@
 import nodemailer from "nodemailer";
-import dns from "dns/promises";
-import net from "net";
-
-async function testGmailConnection() {
-    try {
-        const addresses = await dns.lookup("smtp.gmail.com", {
-            all: true,
-        });
-
-        console.log("Gmail addresses:", addresses);
-
-        const socket = net.createConnection({
-            host: "smtp.gmail.com",
-            port: 587,
-            family: 4,
-        });
-
-        socket.setTimeout(10000);
-
-        socket.on("connect", () => {
-            console.log("✅ TCP connection to Gmail SMTP succeeded");
-            socket.destroy();
-        });
-
-        socket.on("timeout", () => {
-            console.log("❌ TCP connection to Gmail SMTP timed out");
-            socket.destroy();
-        });
-
-        socket.on("error", (error) => {
-            console.log("❌ TCP connection to Gmail SMTP failed:", error);
-        });
-    } catch (error) {
-        console.error("DNS lookup failed:", error);
-    }
-}
-
-testGmailConnection();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    family: 4,
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
+
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
     },
+
     connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 30000,
@@ -54,10 +17,10 @@ const transporter = nodemailer.createTransport({
 
 transporter.verify()
     .then(() => {
-        console.log("✅ Gmail SMTP connection successful");
+        console.log("✅ Brevo SMTP connection successful");
     })
     .catch((error) => {
-        console.error("❌ Gmail SMTP connection failed:");
+        console.error("❌ Brevo SMTP connection failed:");
         console.error(error);
     });
 
@@ -67,7 +30,7 @@ const sendEmail = async ({
     html
 }) => {
     await transporter.sendMail({
-        from: `"Vedic India" <${process.env.EMAIL_USER}>`,
+        from: `"Vedic India" <${process.env.SMTP_FROM}>`,
         to,
         subject,
         html
